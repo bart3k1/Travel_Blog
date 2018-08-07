33eca13892b0bed75e76f5a5cc5c516e02223131
bart3k1 <bart3k1@gmail.com>
2018-08-07 18:10:11 +0200

    tests added

--- travel/trvl_app/tests.py
+++ travel/trvl_app/tests.py
@@ -2,16 +2,13 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 from trvl_app.forms import AddUserForm
+from trvl_app.models import Travel
 
 
 class LoginPageTests(TestCase):
 
     def test_home_page_status_code(self):
         response = self.client.get('')
-        self.assertEquals(response.status_code, 200)
-
-    def test_index_page_status_code(self):
-        response = self.client.get('/index')
         self.assertEquals(response.status_code, 200)
 
 
@@ -27,7 +24,19 @@ class LogInTest(TestCase):
         response = self.client.post('', self.credentials, follow=True)
         self.assertTrue(response.context['user'].is_active)
 
+    def test_login_bad_pass(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'passsssxxx'}
+        response = self.client.post('', self.credentials, follow=True)
+        self.assertFalse(response.context['user'].is_active)
 
+    def test_login_bad_user(self):
+        self.credentials = {
+            'username': 'testuserxxx',
+            'password': 'passsss'}
+        response = self.client.post('', self.credentials, follow=True)
+        self.assertFalse(response.context['user'].is_active)
 
 
 class AddUserFormTest(TestCase):
@@ -46,20 +55,20 @@ class AddUserFormTest(TestCase):
         form = AddUserForm(data=form_data)
         self.assertFalse(form.is_valid())
 
+    def test_form_short_pass(self):
+        form_data = {'username': 'bob', 'password': '123', 'password_c': '123', 'first_name': 'bobi', 'last_name': 'bobber', 'email': 'bob@bob.pl'  }
+        form = AddUserForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
     def test_form_bad_email(self):
         form_data = {'username': 'bob', 'password': 'test123', 'password_c': 'test', 'first_name': 'bobi', 'last_name': 'bobber', 'email': 'bobbob.pl'  }
         form = AddUserForm(data=form_data)
         self.assertFalse(form.is_valid())
 
 
-# class CityFormTest(TestCase):
-#
-#     def test_form_ok(self):
-#         form_data = {'name': 'London', 'date': '2018-08-07 15:00:00'}
-#         form = CityForm(data=form_data)
-#         self.assertTrue(form.is_valid())
-#
-#     def test_form_no_name(self):
-#         form_data = {'name': '', 'date': '2018-08-07 15:00:00'}
-#         form = CityForm(data=form_data)
-#         self.assertFalse(form.is_valid())
+class AddTravelFormTest(TestCase):
+
+    def test_add_travel_post(self):
+        test_user = User.objects.create(username="newuser", password="securetestpassword")
+        Travel.objects.create(author=test_user, topic="Super Important Test", content="This is really important.")
+        self.assertEqual(Travel.objects.last().topic, "Super Important Test")
